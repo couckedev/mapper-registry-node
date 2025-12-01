663f6541c30b71145c923e03286c714835d73795
Anthony COUCKE <anthony.coucke@dornach.eu>
2025-12-01 10:25:06 +0100

    chore: export MapperRegistryItem type

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ export * from './errors/mapper-already-exists.error.js';
 export * from './errors/mapper-not-found.error.js';
 export * from './helpers/create-mapper.function.js';
 export * from './mapper-registry.js';
-export type * from './types/map-function.type.js';
+export * from './types/map-function.type.js';
+export * from './types/mapper-registry-item.type.js';
